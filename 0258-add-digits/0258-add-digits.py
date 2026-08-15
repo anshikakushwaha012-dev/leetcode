6c054f3a7ed8a2db5a60b1f12053bd8ab76f2225
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-08-15 08:50:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.06%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num==0:
+            return 0
+        while num >= 10:
+            total = 0
+            while num > 0:
+                total += num % 10
+                num //= 10
+            num = total
+
+        return num
+        

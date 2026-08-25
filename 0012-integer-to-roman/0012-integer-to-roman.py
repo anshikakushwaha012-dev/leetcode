f648dd02629985f41642a647529cded8b4a973f3
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-08-25 23:22:06 +0530

    Time: 9 ms (18.38%), Space: 19.2 MB (91.49%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values=[(1000,"M"),
+            (900,"CM"),
+            (500,"D"),
+            (400,"CD"),
+            (100,"C"),
+            (90,"XC"),
+            (50,"L"),
+            (40,"XL"),
+            (10,"X"),
+            (9,"IX"),
+            (5,"V"),
+            (4,"IV"),
+            (1,"I")]
+        ans=""
+        for value,s in values:
+            while num>=value:
+                ans+=s
+                num-=value
+        return ans

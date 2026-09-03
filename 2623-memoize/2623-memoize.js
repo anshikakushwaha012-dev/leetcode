08923c8a7a3b8fcd855b71b49984d9d7a6583f4a
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-03 07:14:19 +0530

    Time: 267 ms (43.29%), Space: 96.2 MB (25.64%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -4,22 +4,16 @@
  */
 function memoize(fn) {
     let cache = new Map();
-
     return function(...args) {
         let key = JSON.stringify(args);
-
         if (cache.has(key)) {
             return cache.get(key);
         }
-
         let result = fn(...args);
         cache.set(key, result);
-
         return result;
-    
 };
-    return function(...args) {
-        
+    return function(...args) {  
     }
 }
 

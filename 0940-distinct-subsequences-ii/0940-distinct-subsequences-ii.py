cdbf4cf3b250498806a6d51b2f5fe822d2482d4d
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-07 22:19:48 +0530

    Time: 8 ms (73.44%), Space: 19.4 MB (23.96%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -7,6 +7,6 @@ class Solution:
         for i in range(1,len(s)+1):
             ch=ord(s[i-1])-ord('a')
             dp[i]=(2*dp[i-1])%MOD
-            dp[i] =(dp[i]-last[ch]) % MOD
-            last[ch] = dp[i - 1]
-        return (dp[len(s)] - 1) % MOD
+            dp[i]=(dp[i]-last[ch])%MOD
+            last[ch]=dp[i-1]
+        return (dp[len(s)]-1)%MOD

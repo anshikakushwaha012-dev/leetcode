0fb289eec8114d68a4513f774e8021354be6eeda
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-12 06:31:26 +0530

    Time: 1570 ms (45.76%), Space: 143.2 MB (22.03%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.py
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.py
@@ -2,47 +2,28 @@ class Solution:
     def maximumWeight(self, intervals: List[List[int]]) -> List[int]:
         import bisect
         from functools import lru_cache
-
         n = len(intervals)
-
         arr = []
         for i in range(n):
             l, r, w = intervals[i]
             arr.append((l, r, w, i))
-
         arr.sort()
-
         starts = [x[0] for x in arr]
-
         next_idx = [0] * n
-
         for i in range(n):
             next_idx[i] = bisect.bisect_right(starts, arr[i][1])
-
         @lru_cache(None)
         def dp(i, k):
             if i == n or k == 0:
                 return (0, ())
-
-            # Skip current interval
             not_take = dp(i + 1, k)
-
-            # Take current interval
             l, r, w, idx = arr[i]
-
             score, indices = dp(next_idx[i], k - 1)
-
             new_indices = tuple(sorted((idx,) + indices))
             take = (w + score, new_indices)
-
-            # Choose higher score
             if take[0] > not_take[0]:
                 return take
-
             if take[0] < not_take[0]:
                 return not_take
-
-            # Same score -> lexicographically smaller indices
             return min(take, not_take)
-
         return list(dp(0, 4)[1])

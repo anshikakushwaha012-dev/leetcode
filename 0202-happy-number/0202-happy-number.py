8763b703b6e29681946f253bd2d837710921ae2f
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-13 08:33:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.52%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1:
+            if n in seen:
+                return False
+
+            seen.add(n)
+
+            answer = 0
+
+            while n > 0:
+                last_digit = n % 10
+                answer = answer + last_digit * last_digit
+                n = n // 10
+
+            n = answer
+
+        return True

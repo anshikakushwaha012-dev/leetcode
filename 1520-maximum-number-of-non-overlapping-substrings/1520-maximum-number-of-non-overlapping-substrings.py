52c1cbbc3522c2eeae9dee5f210c88724ac9ba1a
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-18 08:06:44 +0530

    Time: 356 ms (28.76%), Space: 20.4 MB (70.8%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> list[str]:
+        first = [len(s)] * 26
+        last = [-1] * 26
+
+        for i in range(len(s)):
+            x = ord(s[i]) - ord('a')
+            first[x] = min(first[x], i)
+            last[x] = i
+
+        intervals = []
+
+        for i in range(len(s)):
+            x = ord(s[i]) - ord('a')
+
+            if i != first[x]:
+                continue
+
+            left = i
+            right = last[x]
+            j = left
+            valid = True
+
+            while j <= right:
+                y = ord(s[j]) - ord('a')
+
+                if first[y] < left:
+                    valid = False
+                    break
+
+                right = max(right, last[y])
+                j += 1
+
+            if valid:
+                intervals.append((left, right))
+
+        intervals.sort(key=lambda x: x[1])
+
+        ans = []
+        end = -1
+
+        for left, right in intervals:
+            if left > end:
+                ans.append(s[left:right + 1])
+                end = right
+
+        return ans

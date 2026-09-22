586458b1567c366efb0d8d5f5db19403d29ac953
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-22 10:09:00 +0530

    Time: 0 ms (100%), Space: 11.5 MB (98.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,7 +5,6 @@ public:
         int count1 = 0;
         int count2 = 0;
         int n = nums.size();
-
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 count0++;
@@ -17,21 +16,17 @@ public:
                 count2++;
             }
         }
-
         int index = 0;
-
         while (count0 > 0) {
             nums[index] = 0;
             count0--;
             index++;
         }
-
         while (count1 > 0) {
             nums[index] = 1;
             count1--;
             index++;
         }
-
         while (count2 > 0) {
             nums[index] = 2;
             count2--;

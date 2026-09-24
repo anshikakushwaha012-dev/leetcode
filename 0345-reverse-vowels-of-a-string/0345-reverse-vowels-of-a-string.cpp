644f3f03bafb44b0256812d65a6f1bf8692062f1
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-24 09:45:07 +0530

    Time: 0 ms (100%), Space: 10.3 MB (47.98%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string v="";
-        for(int i=0;i<s.length();i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' ||
-               s[i]=='o' || s[i]=='u' ||
-               s[i]=='A' || s[i]=='E' || s[i]=='I' ||
-               s[i]=='O' || s[i]=='U') {
-                v+=s[i];
+      int i=0;
+      int j=s.length()-1;
+      while(i<j){
+         while(i<j && !(s[i]=='a' || s[i]=='e' || s[i]=='i' ||
+                             s[i]=='o' || s[i]=='u' ||
+                             s[i]=='A' || s[i]=='E' || s[i]=='I' ||
+                             s[i]=='O' || s[i]=='U')) {
+                i++;
             }
-        }
-        reverse(v.begin(),v.end());
-        int j=0;
-        for(int i=0;i<s.length();i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' ||
-               s[i]=='o' || s[i]=='u' ||
-               s[i]=='A' || s[i]=='E' || s[i]=='I' ||
-               s[i]=='O' || s[i]=='U'){
-                s[i]=v[j];
-                j++;
+
+            while(i<j && !(s[j]=='a' || s[j]=='e' || s[j]=='i' ||
+                             s[j]=='o' || s[j]=='u' ||
+                             s[j]=='A' || s[j]=='E' || s[j]=='I' ||
+                             s[j]=='O' || s[j]=='U')) {
+                j--;
             }
-        }
-        return s;
+        swap(s[i],s[j]);
+        i++;
+        j--;
+      }  
+      return s;
     }
 };

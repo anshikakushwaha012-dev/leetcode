6b6dcb2cf72eab97bcd12b7f804a513d1d17f68c
Anshika Kushwaha <anshikakushwaha012@gmail.com>
2026-09-25 11:46:02 +0530

    Time: 0 ms (100%), Space: 10.4 MB (17.98%) - LeetHub

---
+++ 2496-maximum-value-of-a-string-in-an-array/2496-maximum-value-of-a-string-in-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumValue(vector<string>& strs) {
+        int ans = 0;
+
+        for(string s : strs) {
+            bool digit = true;
+
+            for(char c : s) {
+                if(c < '0' || c > '9') {
+                    digit = false;
+                    break;
+                }
+            }
+
+            int value;
+
+            if(digit) {
+                value = stoi(s);
+            }
+            else {
+                value = s.length();
+            }
+
+            ans = max(ans, value);
+        }
+
+        return ans;
+    }
+};
